0e88276a0528ece7369ebf88a88ec8de7d8e4153
thaya2218-bit <thaya2218@gmail.com>
2026-04-22 13:41:53 +0530

    task2

---
+++ script.js
@@ -0,0 +1,110 @@
+// Task 1 - Variable Update
+
+console.log("Task 1 - Variable Update");
+
+var num = 50;
+num = 100;
+console.log(num);
+
+// Task 2 - Task 2: Let Variable Math
+
+console.log("Task 2 - Let Variable Math"); 
+
+let marks = 80;
+marks = marks + 10;
+console.log(marks);
+
+// Task 3 - Const Value
+
+console.log("Task 3 - Const Value");
+
+const price = 500;
+let finalPrice = price + 100;
+console.log(finalPrice);
+
+// Task 4: Printing Statements
+
+console.log("Task 4 - Printing Statements");
+
+console.log("Welcome Team");
+console.log(2026);
+console.log(true);
+
+// Task 5: Datatype Check
+
+console.log("Task 5 - Datatype Check");
+
+console.log(typeof "JavaScript");
+console.log(typeof 250);
+console.log(typeof false);
+
+// Task 6: Array Task
+
+console.log("Task 6 - Array Task");
+
+let fruits = ["Apple", "Banana", "Mango", "Orange", "Grapes"];
+
+console.log(fruits);       
+console.log(fruits[0]);       
+console.log(fruits[fruits.length - 1]);
+
+// Task 7: Object Task
+
+console.log("Task 7 - Object Task");
+
+let student = {
+  name: "Naveen",
+  age: 20,
+  course: "MERN"
+};
+
+console.log(student.name);
+console.log(student.age);
+
+// Task 8: Arithmetic Operators
+
+console.log("Task 8 - Arithmetic Operators");
+
+console.log(20 + 10);
+console.log(50 - 25);
+console.log(5 * 5);
+console.log(100 / 4);
+console.log(20 % 3);
+
+// Task 9: Increment / Decrement
+
+console.log("Task 9 - Increment / Decrement");
+
+let x = 5;
+x++;
+console.log(x);
+
+let y = 10;
+y--;
+console.log(y);
+
+// Task 10: Comparison Operators
+
+console.log("Task 10 - Comparison Operators");
+
+console.log(10 > 5);
+console.log(5 < 2);
+console.log(20 == "20");
+console.log(15 === "15");
+console.log(10 != 8);
+
+// Task 11: Logical Operators
+
+console.log("Task 11 - Logical Operators");
+
+console.log(5 > 2 && 10 > 3);
+console.log(7 < 5 || 8 > 2);
+console.log(!(10 > 5));
+
+// Task 12: Ternary Operator
+
+console.log("Task 12 - Ternary Operator");
+
+let age = 18;
+
+console.log(age >= 18 ? "Eligible" : "Not Eligible");
